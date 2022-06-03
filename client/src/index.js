ae097ee078ad94be3875ec4054c05af3755a737e
Jeyhun085 <asgarov.jeyhun@mail.ru>
2022-06-03 10:29:10 +0400

    React script updated

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom';
-import store from './redux/store'
-import App from './Components/App';
+import store from './redux/store.js'
+import App from './Components/App.jsx';
 const container = document.getElementById('app');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
